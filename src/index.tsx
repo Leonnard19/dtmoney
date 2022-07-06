fda9f2d6e06d28c1de6b06378d6ea2b797ac6661
Leonardo <srleon19@hotmail.com>
2022-07-06 20:37:25 -0300

    setup MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createAt: new Date(),
+        },
+      ];
+    });
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
     <App />
